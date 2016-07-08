c8f12be327893482adcf366215cf78c43c7c577c
DimaNovik <dimanovik1989@gmail.com>
2016-07-08 09:45:09 +0300

    Preparation for layout. Added bootstrap grid and media css

--- application/views/View_index.php
+++ application/views/View_index.php
@@ -1,1 +1,54 @@
-<h1>Привет <?php echo $data[0]; ?>!</h1>
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Контрольные обходы</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="application/views/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="application/views/css/main.css" />
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+
+    <![endif]-->
+
+</head>
+
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-2 col-sm-2">
+
+        </div>
+
+        <div class="col-lg-6 col-lg-6 col-sm-6">
+
+            <h1>Привет <?php echo $data[0]; ?>! <a href="tasks"/>pflfybz</a></h1>
+
+        </div>
+    </div>
+
+</div>
+<!-- /.container -->
+
+<!-- jQuery -->
+<script src="application/views/js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="application/views/js/bs.js"></script>
+
+</body>
+
+</html>
